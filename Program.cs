b0f78c52f52ae0b00226015c88d7834efbed757e
刘跃灿 <995611928@qq.com>
2019-08-29 16:46:07 +0800

    C#操作XmL

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace ConsoleApplication
 {
@@ -47,9 +48,83 @@ namespace ConsoleApplication
             dynamic test5 = null;
             dynamic test6 = null;
             Console.WriteLine($"参数1参数2都为空，最终取得值是：{ test5??test6}");
-            #endregion           
-        }
+            #endregion
+            #region 模块三：C#操作Xml文档
+            /* KeyNote:
+             *      1:CreateXmlDeclaration()方法的第三个参数 standalone的汉意：值：yes/no 
+             *        yes:表示不依赖任何外部定义的DTD(文档类型定义目的以某个模板来校验你写的XML)；
+             *        no;表示该Xml文档不是独立的，引用了外部的DTD规范文档；
+             */
+            Console.WriteLine("---------------------C#操作Xml文档--------------------");
+            //写XML文件
+            XmlDocument _xmldocument = new XmlDocument();
+            XmlDeclaration _xmldeclaration = _xmldocument.CreateXmlDeclaration("1.0", "utf-8", "yes");
+            _xmldocument.AppendChild(_xmldeclaration);
+            //根节点
+            XmlElement _persion = _xmldocument.CreateElement("Persion");
+            _persion.SetAttribute("公司", "XXX");
+            _xmldocument.AppendChild(_persion);
+            //附加成员
+            XmlElement _persion1 = _xmldocument.CreateElement("Persion1");
+            _persion.AppendChild(_persion1);
 
+            XmlElement _persion1_1 = _xmldocument.CreateElement("姓名");
+            _persion1_1.InnerText = "gamecc666";
+            _persion1.AppendChild(_persion1_1);
+            XmlElement _persion1_2 = _xmldocument.CreateElement("性别");
+            _persion1_2.InnerText = "男";
+            _persion1.AppendChild(_persion1_2);
+            XmlElement _persion1_3 = _xmldocument.CreateElement("年龄");
+            _persion1_3.InnerText = "27";
+            _persion1.AppendChild(_persion1_3);
+
+            XmlElement _persion2 = _xmldocument.CreateElement("Persion2");
+            _persion.AppendChild(_persion2);
+
+            XmlElement _persion2_1 = _xmldocument.CreateElement("姓名");
+            _persion2_1.InnerText = "xiaoyu";
+            _persion2.AppendChild(_persion2_1);
+            XmlElement _persion2_2 = _xmldocument.CreateElement("性别");
+            _persion2_2.InnerText = "女";
+            _persion2.AppendChild(_persion2_2);
+            XmlElement _persion2_3 = _xmldocument.CreateElement("年龄");
+            _persion2_3.InnerText = "28";
+            _persion2.AppendChild(_persion2_3);
+            //保存Xml文件
+            _xmldocument.Save("gamecc666.xml");
+
+            //读Xml文件
+            //Console.WriteLine("读取xml文件");
+            //XmlDocument _xmldocument = new XmlDocument();
+            //_xmldocument.Load("gamecc666.xml");
+            ////获得根节点的第二种方法：_xmldocument.DocumentElement.Name
+            //XmlNode _xmlnode = _xmldocument.SelectSingleNode("Persion");
+            ////获得根节点的信息
+            //Console.WriteLine("name:" + _xmlnode.Name + "||属性公司：" +((XmlElement)_xmlnode).GetAttribute("公司"));
+            ////获得所有的孩子节点的List集合
+            //XmlNodeList _nodelist = _xmlnode.ChildNodes;
+            //foreach(XmlNode item in _nodelist)
+            //{
+            //    string _persioninfo = "\n";
+
+            //    XmlElement _xmlelementitem = (XmlElement)item;
+            //    _persioninfo += "ID："+_xmlelementitem.Name;
+            //    foreach(XmlNode xn in item )
+            //    {
+            //        var _xmlelementxn = (XmlElement)xn;
+            //        _persioninfo += "||" + _xmlelementxn.Name + " : " + _xmlelementxn.InnerText;
+            //        //修改XML文档
+            //        if(_xmlelementxn.InnerText=="xiaoyu")
+            //        {
+            //            _xmlelementxn.InnerText = "小鱼";
+            //        }
+            //    }
+            //    Console.WriteLine("输出个人信息：" + _persioninfo);
+            //}            
+            //_xmldocument.Save("gamecc666.xml");
+            Console.WriteLine("xml文件操作结束！");
+            #endregion
+        }
 
         #region 模块一：测试泛型方法
         private static void Func<T>(T[] arr)
